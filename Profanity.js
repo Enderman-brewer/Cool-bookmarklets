031efeaa0254fec90c92824ed7839759c08f0ac7
Enderman-brewer <152119296+Enderman-brewer@users.noreply.github.com>
2025-05-14 10:32:52 +1000

    Create Profanity.js

---
+++ Profanity.js
@@ -0,0 +1,21 @@
+fetch("https://raw.githubusercontent.com/areebbeigh/profanityfilter/refs/heads/master/profanityfilter/data/badwords.txt")
+  .then(res => res.text())
+  .then(text => {
+    const words = text.split("\n").filter(w => w.trim());
+    const regex = new RegExp("\\b(" + words.map(w => w.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|") + ")\\b", "gi");
+
+    function censorTextNode(node) {
+      const original = node.nodeValue;
+      node.nodeValue = original.replace(regex, match => "*".repeat(match.length));
+    }
+
+    function walk(node) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        censorTextNode(node);
+      } else if (node.nodeType === Node.ELEMENT_NODE && !['SCRIPT', 'STYLE', 'TEXTAREA'].includes(node.tagName)) {
+        for (const child of node.childNodes) walk(child);
+      }
+    }
+
+    walk(document.body);
+  });
